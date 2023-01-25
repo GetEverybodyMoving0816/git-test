40a39ff688c0987e9b40b5de6a3dd80af4c2d00c
weiss <weiss@atguigu.com>
2023-01-25 13:47:51 +0800

    git 第三次提交本地

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("git第二次修改");
+        System.out.println("git第三次修改");
     }
 }
